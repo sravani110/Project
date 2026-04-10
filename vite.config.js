ceebfc8babffca87c12e06552a73d05e12ca6525
sravani110 <sravanivangala889@gmail.com>
2026-04-10 19:06:58 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], test: { coverage: { provider: 'v8', reporter: ['lcov'] } }
+  plugins: [react()], test: { environment: 'jsdom', coverage: { provider: 'v8', reporter: ['lcov'] } }
 })
